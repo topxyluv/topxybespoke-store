25a32aac94dbfe80b398151e3ed6dcededa6c061
topxyluv <topxyluv@gmail.com>
2026-05-25 11:24:51 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function App() {
+  return (
+    <div style={{padding: '50px', textAlign: 'center', fontFamily: 'sans-serif'}}>
+      <h1>TOPXYBESPOKE</h1>
+      <p>Artisan Tailoring - Coming Soon</p>
+    </div>
+  );
+}
