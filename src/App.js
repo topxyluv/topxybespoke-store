be259e4f75f940e063549463f9d83cb87ea1a715
topxyluv <topxyluv@gmail.com>
2026-05-25 20:25:37 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">TOPXYBESPOKE</h1>
+      <p>Site is live and functional.</p>
+    </div>
+  );
+}
+
+export default App;
+
+```
